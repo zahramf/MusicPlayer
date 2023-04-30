b2f552d0a99f2378d0c05a7069a59a4cb4ebb1ad
unknown <zahramusavifakhr@gmail.com>
2023-04-30 10:55:19 +0430

    create music screen

--- lib/screen/music_screen.dart
+++ lib/screen/music_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -25,6 +26,154 @@ class MusicScreen extends StatelessWidget {
             child: Container(
               color: Colors.grey.withOpacity(
                 0.4,
+              ),
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Text(
+                            'Now Playing',
+                            style: TextStyle(fontSize: 28, color: Colors.white),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const CircleAvatar(
+                        radius: 150,
+                        backgroundImage: AssetImage(
+                            'assets/images/Mohsen-Chavoshi-Rahayam-Kon.jpeg'),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Mohsen Chavoshi",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Rayaham Kon",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.favorite,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const ProgressBar(
+                        progress: Duration(seconds: 30),
+                        total: Duration(seconds: 210),
+                        thumbColor: Colors.redAccent,
+                        baseBarColor: Colors.grey,
+                        progressBarColor: Colors.white,
+                        timeLabelTextStyle: TextStyle(color: Colors.white),
+                        thumbGlowColor: Color.fromARGB(55, 140, 98, 98),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.repeat,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.skip_previous,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(
+                              15,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              gradient: const LinearGradient(
+                                colors: [
+                                  Colors.red,
+                                  Colors.black,
+                                ],
+                                begin: Alignment.bottomRight,
+                                end: Alignment.topLeft,
+                              ),
+                            ),
+                            child: const Icon(
+                              Icons.play_arrow,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.skip_next,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.volume_up,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
